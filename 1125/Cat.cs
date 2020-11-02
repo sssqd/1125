d19e2313f1c8a34ab9e47152ea50efde7d321487
sssqd <kostya.kostyushin25225@mail.ru>
2020-11-02 21:41:38 +1000

    Выздоравливайте ♥♥♥

---
+++ 1125/Cat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1125
+{
+    class Cat
+    {
+        public Cat(string name, DateTime birthday)
+        {
+            Name = name;
+            BirthDay = birthday;
+        }
+        public string Name
+        {
+            get; set;
+        }
+        public DateTime BirthDay
+        {
+            get; set;
+        }
+        public void MakeNoise()
+        {
+            Console.WriteLine(Name + " мяукает");
+        }
+        public int GetAge()
+        {
+            return (DateTime.Today - BirthDay).Days / 365;
+        }
+
+    }
+
+}
---
+++ 1125/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1125
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Пейн", new DateTime(2014, 03, 31));
+            Console.WriteLine($"Коту по имени {cat.Name} уже {cat.GetAge()} лет");
+        }
+    }
+}
