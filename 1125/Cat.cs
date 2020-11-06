f0b0bbe0f1f0db73644b62ad3d44d59cf6ae3b47
sssqd <kostya.kostyushin25225@mail.ru>
2020-11-06 20:29:34 +1000

    создан вольер для котов

--- 1125/Cat.cs
+++ 1125/Cat.cs
@@ -5,9 +5,10 @@ using System.Threading.Tasks;
 
 namespace _1125
 {
-    class Cat
+    public class Cat
     {
         byte _hungryStatus;
+        public event EventHandler HungryStatusChanged;
         public Cat(string name, DateTime birthday)
         {
             Name = name;
@@ -16,78 +17,83 @@ namespace _1125
         }
         public string Name
         {
-            get; set;
+            get;
+            set;
+        }
+        public void MakeNoise()
+        {
+            Console.WriteLine($"{Name } мяукает");
         }
         public DateTime BirthDay
         {
             get; set;
         }
-        public void MakeNoise()
-        {
-            Console.WriteLine(Name + " мяукает");
-        }
         public int GetAge()
         {
             return (DateTime.Today - BirthDay).Days / 365;
         }
-       
-        public void GetStatus()
-        {
-            Console.WriteLine(Name);
-            Console.WriteLine($"Возрост: {GetAge()}");
-            if (HungryStatus < 10)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("Кот умерает от голода ");
-            }
-            else if (HungryStatus >= 10 && HungryStatus <= 40)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Кот очень голоден");
-            }
-            else if (HungryStatus > 40 && HungryStatus <= 70)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine($"Кот хочет кушать");
-            }
-            else if (HungryStatus > 70 && HungryStatus <= 90)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"Кот не против перекусить");
-            }
-            else if (HungryStatus > 90)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"Кот недавно поел");
-            }
-            Console.ResetColor();
-        }
-        async Task LifeCircle()
-        {
-            await Task.Delay(1000);
-            HungryStatus -= 10;
-            await LifeCircle();
-        }
-        public void Feed()
-        {
-            HungryStatus = 100;
-        }
-        public event EventHandler HungryStatusChanged;
         public byte HungryStatus
         {
             get { return _hungryStatus; }
             set
             {
-                if (value < 0)
-                    _hungryStatus = 0;
-                else if (value > 100)
-                    _hungryStatus = 100;
+                byte status = value;
+                if (status < 0)
+                {
+                    status = 0;
+                }
+                else if (status > 100)
+                {
+                    status = 100;
+                }
                 else
                     _hungryStatus = value;
                 if (_hungryStatus != value)
+                {
                     HungryStatusChanged?.Invoke(this, null);
-
+                }
             }
+        }
+        public void Feed(byte needFood)
+        {
+            HungryStatus += needFood;
+        }
+        public void GetStatus()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine($"Возраст: {GetAge()}");
+            if (HungryStatus <= 10)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Кот умирает от голода");
+            }
+            else if (HungryStatus > 10 && HungryStatus <= 40)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Кот очень голоден");
+            }
+            else if (HungryStatus > 40 && HungryStatus <= 70)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Кот хочет кушать");
+            }
+            else if (HungryStatus > 40 && HungryStatus <= 70)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Кот не против перекусить");
+            }
+            else if (HungryStatus > 90)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Кот недавно поел");
+            }
+            Console.ResetColor();
+        }
+        async Task LifeCircle()
+        {
+            await Task.Delay(100);
+            HungryStatus -= 10;
+            await LifeCircle();
         }
     }
 }
---
+++ 1125/CatSmartHouse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1125
+{
+    class CatSmartHouse
+    {
+        List<Cat> cats = new List<Cat>();
+        public CatSmartHouse(int foodResourse)
+        {
+            FoodResourse = foodResourse;
+        }
+        public int FoodResourse
+        { get; set; }
+
+        public void AddCat(Cat cat)
+        {
+            cats.Add(cat);
+            cat.HungryStatusChanged += Cat_HungryStatusChanged;
+        }
+        private void Cat_HungryStatusChanged(object sender, EventArgs e)
+        {
+            var cat = (Cat)sender;
+            if (cat.HungryStatus <= 20 && FoodResourse > 0)
+            {
+                byte needFood = (byte)(100 - cat.HungryStatus);
+                if (FoodResourse > needFood)
+                    FoodResourse -= needFood;
+                else
+                {
+                    needFood = (byte)FoodResourse;
+                    FoodResourse = 0;
+                }
+                cat.Feed(needFood);
+                Console.WriteLine($"Покормлена кошка: {cat.Name}\n Остаток еды в вольере: {FoodResourse}");
+            }
+        }
+    }
+}
